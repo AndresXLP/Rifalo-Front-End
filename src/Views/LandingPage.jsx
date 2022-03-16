518fb70d7172c0240126c1abe493b98d5372fa8c
Andres Puello <apuello1025@gmail.com>
2022-03-16 11:08:34 -0500

    Fix Little Bugs

--- src/Views/LandingPage.jsx
+++ src/Views/LandingPage.jsx
@@ -1,7 +1,7 @@
 import './AllViews.css';
 import placeholderImage from '../image/placeholder-513.webp';
 import { useEffect } from 'react';
-import { Card, Button, Row, Col, Placeholder } from 'react-bootstrap';
+import { Card, Row, Col, Placeholder } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
